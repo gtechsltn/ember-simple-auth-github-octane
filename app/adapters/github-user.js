d17e9d01c442adfd7235d1bf772d32a692adbac3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-04-30 21:53:30 -0400

    Remove authorizer

--- app/adapters/github-user.js
+++ app/adapters/github-user.js
@@ -1,6 +1,13 @@
-import GitHubUserAdapter from "ember-data-github/adapters/github-user";
+/* eslint camelcase: 0 */
+import { isPresent } from "@ember/utils";
 import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
+import GitHubUserAdapter from "ember-data-github/adapters/github-user";
 
 export default GitHubUserAdapter.extend(DataAdapterMixin, {
-  authorizer: "authorizer:github"
+  authorize(xhr) {
+    const { access_token } = this.session.data.authenticated;
+    if (isPresent(access_token)) {
+      xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
+    }
+  }
 });
